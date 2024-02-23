44db7e7dbc055488e89a0ef955f9b2bba27ca9eb
Costin Smilovici <costinsf11@gmail.com>
2024-02-23 14:14:37 -0800

    Replace frontend environment with Vite

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
-import video from './video-placeholder.png';
-import audio from './audio-placeholder.png';
-import './App.css';
+import video from './assets/video-placeholder.png';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="flex flex-col justify-center items-center bg-stone-800 min-h-screen overflow-hidden font-sans">
-      {/* <header className="App-header"> */}
       <h1 className="text-5xl font-bold text-slate-200">Voice Compare</h1>
       <img src={video} alt="video-player" className="size-2/5 max-w-2xl"/>
       <audio id="song" class="w-2/5 max-w-2xl mx-auto" controls>
@@ -15,9 +16,8 @@ function App() {
         <button className='rounded-lg bg-blue-950 p-2 text-slate-200 text-xl'>Hello</button>
         <button className='rounded-lg bg-blue-950 p-2 text-slate-200 text-xl'>World</button>
       </div>
-      {/* </header> */}
     </div>
   );
 }
 
-export default App;
+export default App
