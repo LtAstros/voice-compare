47c5d6e0fe71bf294676387b844107c4157cd311
Costin Smilovici <costinsf11@gmail.com>
2024-02-23 19:05:53 -0800

    Added component file for record button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
 import video from './assets/video-placeholder.png';
 import { useState } from 'react'
 import './App.css'
+import RecordingButton from './components/RecordingButton';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [record, setRecord] = useState("start")
 
   return (
     <div className="flex flex-col justify-center items-center bg-stone-800 min-h-screen overflow-hidden font-sans">
       <h1 className="text-5xl font-bold text-slate-200">Voice Compare</h1>
       <img src={video} alt="video-player" className="size-2/5 max-w-2xl"/>
-      <audio id="song" class="w-2/5 max-w-2xl mx-auto" controls>
-        <source src="https://open.spotify.com/track/7DE0I3buHcns00C0YEsYsY?si=5e0442c12f514f04" type="audio/mpeg"/>
+      <audio id="song" className="w-2/5 max-w-2xl mx-auto" controls>
+        <source type="audio/mpeg"/>
       </audio>
       <div className='grid grid-cols-2 gap-4 mt-4 size-2/5 max-w-2xl'>
-        <button className='rounded-lg bg-blue-950 p-2 text-slate-200 text-xl'>Hello</button>
-        <button className='rounded-lg bg-blue-950 p-2 text-slate-200 text-xl'>World</button>
+        <RecordingButton></RecordingButton>
+        <button className='rounded-lg bg-blue-700 hover:bg-blue-950 p-2 text-slate-200 text-xl'>World</button>
       </div>
     </div>
   );
